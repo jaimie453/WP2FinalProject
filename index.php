c255670ce2c6973a44b83ab4f93e29373d218777
Jaimie <jaimie6453@gmail.com>
2021-04-02 18:00:39 -0400

    added components

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include 'components/head.php'; ?>
+
+    <title>Home</title>
+</head>
+
+<body>
+    <header>
+        <?php include_once 'components/navbar.php'; ?>
+    </header>
+    <p>hewwo</p>
+</body>
+
+</html>
