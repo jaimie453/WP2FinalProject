c71d5b1f5ec25cac3b11f84c98f94b7318fe57cf
Jaimie <jaimie6453@gmail.com>
2021-04-18 14:11:22 -0400

    removed comment

--- components/toast.php
+++ components/toast.php
@@ -1,7 +1,6 @@
 <div id="toast-container">
     <div class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
-            <!-- <img src="..." class="rounded me-2" alt="..."> -->
             <span class="toast-title me-auto">Everyone Travels</span>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
